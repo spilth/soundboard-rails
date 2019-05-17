ff01d38734e2f7b8c8fc58170f718cc1eb8d3639
Brian Kelly <polymonic@gmail.com>
2019-05-17 19:49:14 -0400

    Sounds require a title and an audio file to be valid

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,3 +1,23 @@
 class Sound < ApplicationRecord
   has_one_attached :audio
+
+  validates :title, presence: true
+  validate :audio_presence
+  validate :audio_content
+
+  private
+
+  def audio_presence
+    return if audio.attached?
+
+    errors.add(:audio, 'is required')
+  end
+
+  def audio_content
+    return unless audio.attached?
+    return if audio.blob.content_type.start_with? 'audio/'
+
+    audio.purge_later
+    errors.add(:audio, 'needs to be an audio file')
+  end
 end
